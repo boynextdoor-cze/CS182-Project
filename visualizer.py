baf7e11a485af7b6ce047c837f295b2ee5e9f4b7
boynextdoor <chize@shanghaitech.edu.cn>
2022-10-28 22:00:12 +0800

    Added ImageNet_LT dataset visualization

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 import os
 from matplotlib import pyplot as plt
-import pickle
+import pickle, csv
+import numpy as np
 
 DIRNAME = os.path.dirname(__file__)
 DATAPATH = os.path.join(DIRNAME, 'data')
@@ -26,5 +28,30 @@ def CIFAR(dataset):
     plt.bar(label_count.keys(), label_count.values())
     plt.show()
 
+def ImageNet():
+    path=os.path.join(DATAPATH,'ImageNet_LT','ImageNet_LT_train.txt')
+    num_per_cls=defaultdict(lambda: 0)
+
+    with open(path,'r') as f:
+        trainData=csv.reader(f)
+        for row in trainData:
+            tmp=row[0].split()
+            cls_id=tmp[1]
+            num_per_cls[cls_id]+=1
+
+    num_per_cls={k: v for k, v in sorted(num_per_cls.items(), key=lambda x: x[1],reverse=True)}
+    cls_ids=range(len(num_per_cls.keys()))
+    cls_nums=list(num_per_cls.values())
+    
+    plt.figure()
+    plt.plot(cls_ids,cls_nums)
+    plt.ylim(0,1400)
+    plt.fill_between(x=cls_ids[:300],y1=0,y2=cls_nums[:300],facecolor='red', alpha=0.5)
+    plt.fill_between(x=cls_ids[300:],y1=0,y2=cls_nums[300:],facecolor='green', alpha=0.5)
+    plt.xlabel('Sorted Class Index')
+    plt.ylabel('Number of Instances')
+    plt.savefig('img/ImageNet_LT.jpg',dpi=300)
+
 if __name__ == '__main__':
-    CIFAR('cifar-10')
+    #CIFAR('cifar-10')
+    ImageNet()
